0c5290ab389503d3edbc1747e0dbee2c57516336
anuzcan <pnunezagro@gmail.com>
2021-07-06 16:32:23 -0600

    update

--- layerMake.py
+++ layerMake.py
@@ -1,29 +1,24 @@
+import math 
 from qgis import utils
+from qgis.gui import QgsRubberBand
 from qgis.core import (Qgis, 
     QgsProject, QgsWkbTypes, 
     QgsPointXY, QgsFeature, QgsGeometry, 
     QgsField, QgsCoordinateReferenceSystem, 
-    QgsCoordinateTransform)
+    QgsCoordinateTransform,
+    QgsPoint)
 from PyQt5.QtCore import QVariant
+from PyQt5.QtGui import QColor
 
 
 class layerMake:
-	def __init__(self,layer, point_ID = 'survey', timeSurveyPoint=0, filt = 'SINGLE'):
+	def __init__(self,layer, point_ID = 'survey', filt = 'SINGLE'):
 		
 		self.layer_to_edit = layer
 		layer_type = self.layer_to_edit.geometryType()
 		self.count = 0
 
-		self.timeSurveyPoint = timeSurveyPoint
-		self.timeComplete = timeSurveyPoint
-		self.SurveyPointEnabled = True
 		self.filterPoint = self.setfilter(filt)
-		self.porcent = 0
-		self.pointName = point_ID
-
-		self.latPoint = []
-		self.lonPoint = []
-		self.altPoint = []
 		
 		#print(self.layer_to_edit)
 		#print(layerEPSG)
@@ -91,31 +86,7 @@ class layerMake:
 			utils.iface.mapCanvas().refresh()
 
 			self.count += 1
-		
-	def collect_point(self,date,time,x,y,alt,fix_mode,sat_n):
-
-		if self.timeSurveyPoint > 0:
-			if fix_mode in self.filterPoint:
-				self.latPoint.append(y)
-				self.lonPoint.append(x)
-				self.altPoint.append(alt)
-
-				self.timeSurveyPoint -= 1
-				self.porcent = ((self.timeComplete - self.timeSurveyPoint)/self.timeComplete)*100
-
-		elif self.timeSurveyPoint <= 0:
-			self.add_point(date,
-				time,
-				sum(self.lonPoint)/len(self.lonPoint),
-				sum(self.latPoint)/len(self.latPoint),
-				sum(self.altPoint)/len(self.altPoint),
-				fix_mode,
-				sat_n,
-				self.pointName)
-
-			self.porcent = 0
-			self.SurveyPointEnabled = False
-		
+	
 
 	def setfilter(self,Filter):
 
@@ -127,3 +98,72 @@ class layerMake:
 
 		elif Filter == 'SINGLE':
 			return [-1,1,5,4]
+
+class direction:
+	def __init__(self, rotation=0, clockwise=False):
+
+		crsSrc = QgsCoordinateReferenceSystem("EPSG:4326")          
+		crsDest = QgsCoordinateReferenceSystem("EPSG:3857")                       # WGS 84 a WGS de la capa seleccionada
+		transformContext = QgsProject.instance().transformContext()             # Crear instancia de tranformacion
+		self.xform = QgsCoordinateTransform(crsSrc, crsDest, transformContext)  # Crear formulario transformacion
+		
+		self.rotation = rotation
+		self.clockwise = clockwise
+
+	def new_point(self, lon, lat):
+		self.pt1 = self.xform.transform(QgsPointXY(lon, lat))
+
+	def distance(self, lon, lat):
+		pt2 = self.xform.transform(QgsPointXY(lon, lat))
+		distance = math.sqrt((pt2[0] - self.pt1[0])**2 + (pt2[1] - self.pt1[1])**2)
+		return distance
+
+	def angle_to(self, lon, lat):
+		pt2 = self.xform.transform(QgsPointXY(lon, lat))
+
+		if abs(self.rotation) > 360:
+			self.rotation %= 360
+
+		Dx = pt2[0] - self.pt1[0]
+		Dy = pt2[1] - self.pt1[1]
+		angle = math.degrees(math.atan2(Dx, Dy))
+
+		if self.clockwise:
+			angle -= self.rotation
+			return angle if angle > 0 else angle + 360
+		else:
+			angle = (360 - angle if angle > 0 else -1 * angle) - self.rotation
+			return angle if angle > 0 else angle + 360
+
+
+def point_pos(origin, amplitude, angle, rotation=0, clockwise=False):
+	if abs(rotation) > 360:
+		rotation %= 360
+	if clockwise:
+		rotation *= -1
+	if clockwise:
+		angle -= rotation
+		angle = angle if angle > 0 else angle + 360
+	else:
+		angle = (360 - angle if angle > 0 else -1 * angle) - rotation
+		angle = angle if angle > 0 else angle + 360
+
+	theta_rad = math.radians(angle)
+	return int(origin[0] + amplitude * math.sin(theta_rad)), int(origin[1] + amplitude * math.cos(theta_rad))
+
+
+# https://stackoverrun.com/es/q/10271498
+
+class guide:
+	def __init__(self,mapCanvas):
+		#print(mapCanvas.mapSettings().destinationCrs().authid())
+		self.r_polyline = QgsRubberBand(mapCanvas, False)
+		self.r_polyline.setWidth(2)
+		self.r_polyline.setColor(QColor(0, 100, 255))
+	
+	def paint(self):
+		points = [QgsPoint(-85, 10), QgsPoint(-84, 10.5), QgsPoint(-84, 10.4)]
+		self.r_polyline.setToGeometry(QgsGeometry.fromPolyline(points), None)
+    
+	def erase(self):
+		self.r_polyline.reset(QgsWkbTypes.LineGeometry)
--- main_survey.py
+++ main_survey.py
@@ -9,13 +9,11 @@ from PyQt5.QtWidgets import QAction, QMessageBox
 from qgis import utils
 from qgis.core import Qgis, QgsApplication, QgsProject
 
-
 #Importacion metodos
-from .layerMake import layerMake
+from .layerMake import layerMake, direction, point_pos, guide
 
 def classFactory(iface):
     return Main_Plugin(iface)
-
 
 class Main_Plugin:
     def __init__(self, iface):
@@ -38,40 +36,78 @@ class Main_Plugin:
         self.dock.zoomInbutton.clicked.connect(self.zoomInMapCanvas)
         self.dock.zoomOutbutton.clicked.connect(self.zoomOutMapCanvas)
         self.dock.buttonSelectLayer.clicked.connect(self.SelectLayerSurvey)
+        self.dock.buttonGpsActive.clicked.connect(self.star_Read)
 
-        #Definicion de banderas 
+        #Definicion de banderas
+        self.device = False 
         self.flatRotationMap = False
+        self.flatSurveyContinuos = False
+        self.i = True
 
-    def unload(self):
+    def unload(self): 
         self.iface.removeToolBarIcon(self.action)
         del self.action
 
-    def run(self):
+    def run(self): # Iniciamos plugin en interfaz
         
-        self.device = self.testSignal()
-        
+        self.device = self.testSignal() # Se comprueba que se disponga de un dispositivo valido conectado
+        #self.guide = guide(self.iface.mapCanvas())
+        #self.guide.paint()
+        #self.guide.erase()
+
+        self.timerDevice = QTimer()
+        self.timerDevice.timeout.connect(self.testSignal)
+        #self.timerDevice.start(1000)
+        #self.timerDevice.stop()
+
+
         #Si dispositivo conectado, iniciamos contador de lectura de datos
         if self.device == True:
+
             self.timer = QTimer()
             self.timer.timeout.connect(self.read_Device)
             self.timer.start(1000)
 
+            self.rumbo = direction(clockwise=True) # Variable para procesar rumbo y distancias entre puntos
+        
         #Agregamos panel a interface
         self.iface.addDockWidget( Qt.RightDockWidgetArea, self.dock )
-        
+
+        # Bloquemos de botones
+        self.dock.buttonGpsActive.setEnabled(False)
+        self.dock.setRotationButton.setEnabled(False)
+
 
     def testSignal(self):  # Rutina comprobar GPS Correctamento conectado
 
         # Registro del GPS
         self.connectionList = QgsApplication.gpsConnectionRegistry().connectionList()
         
-        if self.connectionList == []:
-            utils.iface.messageBar().pushMessage("Error ","Dispositivo GPS no Conectado",level=Qgis.Critical,duration=5)
+        if self.connectionList == []: # Si no se encuentra ningun dispositivo gps disponible
+            utils.iface.messageBar().pushMessage("Error ","Dispositivo GPS no Conectado",level=Qgis.Critical,duration=3)
             return -1
 
-        else:
-            utils.iface.messageBar().pushMessage("OK ","Dispositivo GPS Encontrado",level=Qgis.Info,duration=5)
+        else:   # Dispositivo gps detectado
+            utils.iface.messageBar().pushMessage("OK ","Dispositivo GPS Encontrado",level=Qgis.Info,duration=3)
+            # Habilitamos rotacion del mapa
+            self.dock.setRotationButton.setEnabled(True)
             return 1
+
+    def SelectLayerSurvey(self): # Seleccionar capa para almacenar los puntos colectados
+
+        # Creamos la instancia de capa a editar con la capa seleccionada en el combobox del panel
+        self.layerSurvey = layerMake(
+            QgsProject().instance().mapLayersByName(self.dock.mMapLayerComboBox.currentText())[0],filt = 'FLOAT')
+
+        if self.layerSurvey.error == False: # si no hay error 
+            utils.iface.messageBar().pushMessage("Correcto "," Capa valida",level=Qgis.Info,duration=5)
+            
+            if self.device == True:
+                self.dock.buttonGpsActive.setEnabled(True) # Habilitamos el inicio de captura
+            
+        else:
+            utils.iface.messageBar().pushMessage("Advertencia "," Capa selecionada no valida",level=Qgis.Warning,duration=5)
+
 
     def read_Device(self): # Rutina captura y almacenamiento de punto en capa
         
@@ -80,10 +116,38 @@ class Main_Plugin:
         except:
             utils.iface.messageBar().pushMessage("Error ","Perdida Conexion",level=Qgis.Critical,duration=10)
             self.timer.stop()
-            
+            self.device = False
+            return -1
+        
+        # Extraer informacion fecha y hora UTC, calidad de resolucion
         now = GPSInformation.utcDateTime.currentDateTime().toString(Qt.TextDate)
-        self.showFix(self.dock.lineEdit,str(GPSInformation.quality))
-    
+        quality = GPSInformation.quality
+        date = now[22:]+'-'+now[5:8]+'-'+now[10:12]
+        time = now[13:21]
+
+        # Mostrar calidad de resolucion de informacion GPS
+        self.showFix(self.dock.lineEdit,str(quality))
+
+        
+        if self.i == True: # condicional para evitar buche de primer dato
+            self.rumbo.new_point(GPSInformation.longitude,GPSInformation.latitude)
+            self.i = False
+        
+        else:
+            # Determinamos angulo y distacion con respecto al punto anterior
+            angulo = self.rumbo.angle_to(GPSInformation.longitude,GPSInformation.latitude)
+            distancia = self.rumbo.distance(GPSInformation.longitude,GPSInformation.latitude)
+
+            if distancia > 1:   # Si la distancia es mayor
+                if self.flatRotationMap == True:
+                    utils.iface.mapCanvas().setRotation(360 - angulo)
+                    self.direc.new_point(GPSInformation.longitude,GPSInformation.latitude)
+                
+                if flatSurveyContinuos == True:
+                    # Almacenamos nuevo punto en la capa seleccionada
+                    self.layerSurvey.add_point(date,time,GPSInformation.longitude,GPSInformation.latitude,GPSInformation.elevation,quality,len(GPSInformation.satPrn))
+                    
+
     def zoomInMapCanvas(self):
         utils.iface.mapCanvas().zoomByFactor(0.8)
 
@@ -113,7 +177,15 @@ class Main_Plugin:
             self.dock.setRotationButton.setStyleSheet("QPushButton{background-color : lightgrey;}")
             self.flatRotationMap = False
 
-    def SelectLayerSurvey(self):
+    
+    def star_Read(self):
+                                                # Rutina inicializar toma de puntos
+        if self.flatSurveyContinuos == False:
+            self.dock.buttonGpsActive.setStyleSheet("QPushButton {background-color : red;}")
+            self.dock.buttonGpsActive.setText('Detener')
+            self.flatSurveyContinuos = True
 
-        self.layerSurvey = layerMake(
-            QgsProject().instance().mapLayersByName(self.dock.mMapLayerComboBox.currentText())[0])
+        else:
+            self.dock.buttonGpsActive.setStyleSheet("QPushButton{background-color : lightgrey;}")
+            self.dock.buttonGpsActive.setText('Captura')
+            self.flatSurveyContinuos = False
