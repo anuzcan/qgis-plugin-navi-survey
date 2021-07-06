fd584354d31f5dbdaefb349a9e2696e9f08e6bc6
anuzcan <pnunezagro@gmail.com>
2021-07-06 10:35:01 -0600

    inicial

---
+++ README.md
@@ -0,0 +1,40 @@
+# QUICK SURVEY RTK QGIS PLUGIN 1.0 ![](icon.png)
+
+Quick_survey_RTK is a Qgis plugin for quick collect points in field
+
+Tool designed to capture points in the field in real time.
+  The collected points are stored in a previously saved layer. The application is in charge of adding the points in the coordinates given under the configured filters.
+  The points will be used for the generation of DEM of the surveyed land, with which level curves can be projected for the establishment of irrigation systems in crops such as rice.
+  
+  ![QGIS](SRTM-en-QGIS.jpg)
+ 
+  For models with acceptable precision, the use of GNSS RTK systems is necessary. Like Emlid Reach RS devices or any other RTK system.
+  
+  The tool is under development and I invite everyone to participate with me in its evolution.
+This tool will help producers to implement current technologies in more efficient irrigation systems.
+
+  ![ATN](logoEmpresa.jpg)
+
+  On Windows, to quickly install the plugin, download repository, unzip and copy the master folder to its equivalent path, or clone the repository in the same path.
+
+    C:\Users\username\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins\
+
+- [x] Configure access to GPS information
+- [X] Set point filters
+- [X] Debug interface
+- [ ] Field tests
+- [ ] Develop help documentation
+- [X] Set rotation map in real time
+- [ ] Project area calculation
+
+### Version Control
+
+- 1.0
+
+## Instructions for use
+
+1) Create a point layer, it can be shp file or temporary layer
+2) Connect the GPS using the GPS information panel in QGIS
+3) Open the plugin
+4) Select the layer where the surveyed points will be saved, as needed in continuous survey or single point
+5) Start capture
---
+++ main_survey.py
@@ -0,0 +1,114 @@
+import os
+
+#Importacion de librerias 
+from PyQt5 import uic
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QAction, QMessageBox
+
+from qgis import utils
+from qgis.core import Qgis, QgsApplication
+
+
+#Importacion metodos
+
+
+def classFactory(iface):
+    return Main_Plugin(iface)
+
+
+class Main_Plugin:
+    def __init__(self, iface):
+        self.iface = iface
+        
+    def initGui(self):
+
+        #Definir icono agregar al interfaz de Qgis
+        path = os.path.dirname( os.path.abspath( __file__ ) )
+        icon_path = os.path.join(path,"icon.png")
+        self.action = QAction(QIcon(icon_path),"&Navegacion Herramienta",self.iface.mainWindow())
+        self.iface.addToolBarIcon(self.action)
+        self.action.triggered.connect(self.run)
+
+        #Cargamos la interfaz grafica
+        self.dock = uic.loadUi( os.path.join( path, "dock.ui" ) )
+        
+        # Creamos acciones para los botones y comandos
+        self.dock.setRotationButton.clicked.connect(self.rotationMap)
+        self.dock.zoomInbutton.clicked.connect(self.zoomInMapCanvas)
+        self.dock.zoomOutbutton.clicked.connect(self.zoomOutMapCanvas)
+
+        #Definicion de banderas 
+        self.flatRotationMap = False
+
+    def unload(self):
+        self.iface.removeToolBarIcon(self.action)
+        del self.action
+
+    def run(self):
+        
+        self.device = self.testSignal()
+        
+        #Si dispositivo conectado, iniciamos contador de lectura de datos
+        if self.device == True:
+            self.timer = QTimer()
+            self.timer.timeout.connect(self.read_Device)
+            self.timer.start(1000)
+
+        #Agregamos panel a interface
+        self.iface.addDockWidget( Qt.RightDockWidgetArea, self.dock )
+        
+
+    def testSignal(self):  # Rutina comprobar GPS Correctamento conectado
+
+        # Registro del GPS
+        self.connectionList = QgsApplication.gpsConnectionRegistry().connectionList()
+        
+        if self.connectionList == []:
+            utils.iface.messageBar().pushMessage("Error ","Dispositivo GPS no Conectado",level=Qgis.Critical,duration=5)
+            return -1
+
+        else:
+            utils.iface.messageBar().pushMessage("OK ","Dispositivo GPS Encontrado",level=Qgis.Info,duration=5)
+            return 1
+
+    def read_Device(self): # Rutina captura y almacenamiento de punto en capa
+        
+        try:
+            GPSInformation = self.connectionList[0].currentGPSInformation()
+        except:
+            utils.iface.messageBar().pushMessage("Error ","Perdida Conexion",level=Qgis.Critical,duration=10)
+            self.timer.stop()
+            
+        now = GPSInformation.utcDateTime.currentDateTime().toString(Qt.TextDate)
+        self.showFix(self.dock.lineEdit,str(GPSInformation.quality))
+    
+    def zoomInMapCanvas(self):
+        utils.iface.mapCanvas().zoomByFactor(0.8)
+
+    def zoomOutMapCanvas(self):
+        utils.iface.mapCanvas().zoomByFactor(1.2)
+    
+    def showFix(self, parent, fix):
+
+        if fix == "1":
+            parent.setText('SINGLE')
+            parent.setStyleSheet("background-color: rgb(255, 0, 0);color: rgb(255, 255, 255);")
+        if fix == "5":
+            parent.setText('FLOAT')
+            parent.setStyleSheet("background-color: rgb(255, 128, 0);color: rgb(255, 255, 255);")
+        if fix == "4":
+            parent.setText('FIX')
+            parent.setStyleSheet("background-color: rgb(0, 255, 0);color: rgb(255, 255, 255);")
+
+    def rotationMap(self):
+
+        if self.flatRotationMap == False:
+
+            self.dock.setRotationButton.setStyleSheet("QPushButton {background-color : green;}")
+            self.flatRotationMap = True
+
+        else:
+            self.dock.setRotationButton.setStyleSheet("QPushButton{background-color : lightgrey;}")
+            self.flatRotationMap = False
+
